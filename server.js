95023a6c3fa4a826552862bafebb15c16c06e037
Vitalii Melnik <mvitalim@gmail.com>
2023-09-14 21:48:57 +0300

    vercel deploy

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv'; // to use env variables
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import router from './router.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+});
 
 app.use(cors());
 app.use(bodyParser.json());
